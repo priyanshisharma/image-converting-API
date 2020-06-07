fdeac85fd1ee39127caa75c7ee619ddfff595df3
priyanshisharma <priyanshi417@gmail.com>
2020-06-08 00:21:29 +0530

    Added Parser

--- ConvertImg/views.py
+++ ConvertImg/views.py
@@ -1,11 +1,13 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import MultiPartParser
 from rest_framework import status
 from .serializers import ImageConversionSerializer
 
 # Create your views here.
 
 @api_view(['POST'])
+@parser_classes([MultiPartParser])
 def image_conversion(request):
 
     serializer = ImageConversionSerializer(data=request.data)
